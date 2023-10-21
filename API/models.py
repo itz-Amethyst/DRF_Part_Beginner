6e6efd71fbde14ad20742ee5a69188e933595894
itz-amethyst <pransermi@gmail.com>
2023-10-21 20:32:11 +0330

    feat:init

---
+++ API/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length = 200, verbose_name = 'عنوان')
+    text = models.TextField(blank = True)
+    is_enable = models.BooleanField(default = False)
+    publish_date = models.DateField(null = True, blank = True)
+    created_date = models.DateTimeField(auto_now_add = True)
+    updated_date = models.DateTimeField(auto_now = True)
+
+    objects = models.Manager()
+    live = PostLiveManager()
+
+
+    def __str__(self):
+        return self.title
---
+++ API/serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from API.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title', 'text', 'is_enable', 'publish_date']
