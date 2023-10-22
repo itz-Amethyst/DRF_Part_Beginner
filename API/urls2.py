c90f3595e62cca496f87df858727def84d62499a
itz-amethyst <pransermi@gmail.com>
2023-10-22 18:20:17 +0330

    fix

--- API/urls2.py
+++ API/urls2.py
@@ -1,8 +1,17 @@
+import requests
+
 from .views import *
 
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('data', PostList, basename = 'data')
-urlpatterns = router.urls
+router.register('post-viewset', PostView, basename = 'post')
+
+urlpatterns = [
+    # path('/postdetail', PostDetailView.as_view(), name='test'),
+    path('', PostListView.as_view(), name='post'),
+    path('/postdetail/<int:pk>', PostDetailView.as_view(), name='test')
+]
+
+urlpatterns += router.urls
