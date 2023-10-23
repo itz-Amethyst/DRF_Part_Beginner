dc0e57f18b38a9a4b4263fb0c5ac8e1b4ad875f1
itz-amethyst <pransermi@gmail.com>
2023-10-23 17:47:28 +0330

    fix

--- Todo_Module/serializer.py
+++ Todo_Module/serializer.py
@@ -3,8 +3,7 @@ from rest_framework import serializers
 from Todo_Module.models import Todo
 
 
-class TodoSerializer(serializers.HyperlinkedModelSerializer):
-
+class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = '__all__'
