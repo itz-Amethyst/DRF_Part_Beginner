1ef7c5bcc2faa4fc6a79ec76b841b5a8ea85f1fb
itz-amethyst <pransermi@gmail.com>
2023-10-23 21:20:55 +0330

    feat: added new prop to model

--- Todo_Module/models.py
+++ Todo_Module/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+user = get_user_model()
 
 class Todo(models.Model):
     title = models.CharField(max_length = 200)
     content = models.TextField()
     priority = models.IntegerField(default = 1)
     is_done = models.BooleanField(default = False)
+    user = models.ForeignKey(to = user, on_delete = models.CASCADE, related_name = 'todos', blank = True, null = True )
 
     def __str__(self):
         return f'{self.title} / Is_Done: {self.is_done}'
--- Todo_Module/serializer.py
+++ Todo_Module/serializer.py
@@ -2,8 +2,19 @@ from rest_framework import serializers
 
 from Todo_Module.models import Todo
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    # Related name in model
+    todos = TodoSerializer(read_only = True, many = True)
+    class Meta:
+        model = User
         fields = '__all__'
