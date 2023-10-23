fae7f23af99bf0d44d9ee3b4490486db6499ea9e
itz-amethyst <pransermi@gmail.com>
2023-10-23 19:06:03 +0330

    feat: Added mixins

--- Todo_Module/urls.py
+++ Todo_Module/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     # path('todo-w-serializer/', all_todos_w_serializer, name='all_todos_w_serializer'),
     # path('todo-detail/<int:todo_id>', todo_detail_view, name='todo_detail_page'),
 
-    path('cbv/', TodosListApiView.as_view(), name='manage-todo'),
-    path('cbv/<int:todo_id>', TodosDetailApiView.as_view(), name='manage-todo'),
+    # path('cbv/', TodosListApiView.as_view(), name='manage-todo'),
+    # path('cbv/<int:todo_id>', TodosDetailApiView.as_view(), name='manage-todo'),
+
+    #! MixIns
+    path('mixins/', TodosListMixinApiView.as_view(), name='mixins'),
+    path('mixins/<pk>', TodosDetailMixinApiView.as_view(), name='mixins_detail'),
 ]
--- Todo_Module/views.py
+++ Todo_Module/views.py
@@ -1,11 +1,10 @@
-from django.http import HttpRequest , JsonResponse
 from django.shortcuts import render
 
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status , permissions
-from rest_framework.decorators import api_view , permission_classes
 from rest_framework.views import APIView
+from rest_framework import generics, mixins
 
 from Todo_Module.models import Todo
 from Todo_Module.serializer import TodoSerializer
@@ -22,44 +21,75 @@ def Index(request):
     return render(request, 'Todo_Module/Index.html', context)
 
 
-class TodosListApiView(APIView):
+# class TodosListApiView(APIView):
+#
+#     permission_classes = [permissions.AllowAny]
+#     def get( self, request: Request ):
+#         todos = Todo.objects.order_by('-priority').all()
+#         todo_serializer = TodoSerializer(todos , many = True)
+#         return Response({'todos': todo_serializer.data} , status = status.HTTP_200_OK)
+#
+#     def post( self, request: Request ):
+#         serializer = TodoSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data , status = status.HTTP_201_CREATED)
+#         return Response(serializer.errors , status.HTTP_400_BAD_REQUEST)
+#
+#
+# class TodosDetailApiView(APIView):
+#     permission_classes = [permissions.AllowAny]
+#
+#     @staticmethod
+#     def get_todo( todo_id:int ):
+#         todo = get_object(object_id = todo_id, object = Todo)
+#         return todo
+#
+#     def get( self, request:Request, todo_id ):
+#         todo = self.get_todo(todo_id)
+#         serializer = TodoSerializer(todo)
+#         return Response(serializer.data, status.HTTP_200_OK)
+#
+#     def put( self , request: Request , todo_id ):
+#         todo = self.get_todo(todo_id)
+#         serializer = TodoSerializer(todo, data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data , status.HTTP_200_OK)
+#         return Response(serializer.errors , status.HTTP_400_BAD_REQUEST)
+#
+#     def delete( self , request: Request , todo_id ):
+#         todo = self.get_todo(todo_id)
+#         todo.delete()
+#         return Response(None, status.HTTP_204_NO_CONTENT)
+#
 
+
+##! Mixin Parts
+
+class TodosListMixinApiView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Todo.objects.order_by('-priority').all()
+    serializer_class = TodoSerializer
     permission_classes = [permissions.AllowAny]
-    def get( self, request: Request ):
-        todos = Todo.objects.order_by('-priority').all()
-        todo_serializer = TodoSerializer(todos , many = True)
-        return Response({'todos': todo_serializer.data} , status = status.HTTP_200_OK)
 
-    def post( self, request: Request ):
-        serializer = TodoSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data , status = status.HTTP_201_CREATED)
-        return Response(serializer.errors , status.HTTP_400_BAD_REQUEST)
+    def get( self, request:Request ):
+        return self.list(request)
+
+    def post( self, request:Request ):
+        return self.create(request)
 
 
-class TodosDetailApiView(APIView):
+class TodosDetailMixinApiView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Todo.objects.order_by('-priority').all()
+    serializer_class = TodoSerializer
     permission_classes = [permissions.AllowAny]
+    # Note in Generic mixins should use pk as id
 
-    @staticmethod
-    def get_todo( todo_id:int ):
-        todo = get_object(object_id = todo_id, object = Todo)
-        return todo
+    def get( self, request:Request , pk):
+        return self.retrieve(request, pk)
 
-    def get( self, request:Request, todo_id ):
-        todo = self.get_todo(todo_id)
-        serializer = TodoSerializer(todo)
-        return Response(serializer.data, status.HTTP_200_OK)
+    def put( self, request:Request, pk ):
+        return self.update(request, pk)
 
-    def put( self , request: Request , todo_id ):
-        todo = self.get_todo(todo_id)
-        serializer = TodoSerializer(todo, data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data , status.HTTP_200_OK)
-        return Response(serializer.errors , status.HTTP_400_BAD_REQUEST)
-
-    def delete( self , request: Request , todo_id ):
-        todo = self.get_todo(todo_id)
-        todo.delete()
-        return Response(None, status.HTTP_204_NO_CONTENT)
+    def delete( self, request:Request, pk ):
+        return self.destroy(request, pk)
